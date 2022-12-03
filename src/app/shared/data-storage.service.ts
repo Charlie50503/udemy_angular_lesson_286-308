d0d66563ac6810d2e8253bb08b4f3d059b02c0dc
DESKTOP-0HRQPN4\営業所 <cate50503@gmail.com>
2022-12-03 18:04:31 +0800

    feat: 添加攔截器並重構fetchRecipes方法

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -23,31 +23,24 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        console.log("user",user);
-
-        return this.http
-          .get<Recipe[]>(
-            // 'https://angular-http-request-dd100-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json?auth=' +user.token // 方法一
-            'https://angular-http-request-dd100-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json', // 方法二
-            {
-              params:new HttpParams().set("auth",user.token)
-            }
-          )
-      }),
-      map(recipes => {
-        return recipes.map(recipe => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : []
-          };
-        });
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      }))//使用 take 後 次數用完了就會解除訂閱
-
+    return this.http
+      .get<Recipe[]>(
+        'https://angular-http-request-dd100-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json'
+      ).pipe(
+        map(recipes => {
+          if(!recipes) {
+            return []
+          }
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
   }
 }
